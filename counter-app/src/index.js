00893a6423823918290b7e8c397f347f25efe524
mallikarjuna4406 <mallikarjuna4406@gmail.com>
2024-10-29 17:35:28 +0530

    - Counter app as a component in shopping applications

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
-import "github-fork-ribbon-css/gh-fork-ribbon.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
